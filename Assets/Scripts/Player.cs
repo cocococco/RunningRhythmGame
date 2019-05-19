6e54ac3cc433a3924501ddc4ccca29e420de23fa
cocococco <kyeonsoo26@gmail.com>
2019-05-20 00:41:24 +0900

    화면 3분할, 클릭 시 해당 트랙으로 위치 이동 (위치는 임의로 지정함)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int screenWidth;
+    public int screenHeight;
+    public Vector3 position1 = new Vector3(-2, -3, 0);
+    public Vector3 position2 = new Vector3(0, -3, 0);
+    public Vector3 position3 = new Vector3(2, -3, 0);
+
+    private void Awake()
+    {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+    }
+
+    private void Start()
+    {
+        Debug.Log(screenWidth);
+        Debug.Log(screenHeight);
+        Debug.Log(this.transform.position);
+        this.transform.position = position2;
+    }
+
+    public void OnClickButton1()
+    {
+        this.transform.position = position1;
+    }
+
+    public void OnClickButton2()
+    {
+        this.transform.position = position2;
+    }
+
+    public void OnClickButton3()
+    {
+        this.transform.position = position3;
+    }
+}
