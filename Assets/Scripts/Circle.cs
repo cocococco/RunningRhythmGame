b99cdf770d25382bef62440b4cf3d0776d18eb28
cocococco <kyeonsoo26@gmail.com>
2019-06-02 20:37:04 +0900

    몬스터 프리팹 구현 - 원이 생기고 시간이 지남에 따라 작아짐 (일단 transparent shader 만들어서 구현했는데, 모델링이나 이펙트 만들어서 입히는 게 나을 듯) 폴더명 수정(Material -> Materials)

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public float speed;
+    public float scale;
+
+    private void Start()
+    {
+        this.transform.localScale = scale * new Vector3(1, 1, 1);
+    }
+
+    private void Update()
+    {
+        this.transform.localScale -= speed * Time.deltaTime * new Vector3(1, 1, 1);
+
+        if (this.transform.localScale.x < 0)
+        {
+            this.transform.localScale = new Vector3(0, 0, 0);
+            Destroy(this.gameObject);
+        }
+    }
+}
