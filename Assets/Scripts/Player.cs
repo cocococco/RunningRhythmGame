b5b21727c361560a29bd952e57a5d8fdf0358a67
cocococco <kyeonsoo26@gmail.com>
2019-06-17 22:35:14 +0900

    몬스터 삭제에 따른 각각의 점수 증가 (다른 곳 클릭 시 gradeScore 초기화 안되는 버그 해결해야 함)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,6 @@
 using System.Collections;
 using DG.Tweening;
 using UnityEngine.UI;
-using System;
-
-public static class Constants
-{
-    public const int contPerSec = 10;
-}
 
 public class Player : MonoBehaviour
 {
@@ -15,34 +9,6 @@ public class Player : MonoBehaviour
     public float posX2 = 0;
     public float posX3 = 2;
     public float speed = 0.5f;
-
-    private float timer;
-    private int distance;
-    private int score;
-    public Text distanceText;
-    public Text scoreText;
-
-    private void Awake()
-    {
-        timer = 0;
-    }
-
-    private void MakeTimer()
-    {
-        for (int i = 0; i < Constants.contPerSec; i++)
-        {
-            timer += Time.deltaTime;
-            distance = Mathf.FloorToInt(timer);
-            score = distance * 100;
-            distanceText.text = distance.ToString() + "M";
-            scoreText.text = score.ToString();
-        }
-    }
-
-    private void Update()
-    {
-        MakeTimer();
-    }
 
     public void OnClickTrackButton1()
     {
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private static Score instance;
+
+    public static Score GetInstance()
+    {
+        return instance;
+    }
+
+    //왜 안되는지 확인
+    //public static Score GetScore
+    //{
+    //    get
+    //    {
+    //        if (i_score == null)
+    //        {
+    //            i_score = GameObject.FindObjectOfType(typeof(Score)) as Score;
+    //            if (i_score == null)
+    //            {
+    //                Debug.LogError("There's no active Score object");
+    //            }
+    //        }
+
+    //        return i_score;
+    //    }
+    //}
+
+    private float timer;
+    private int distance;
+    private int totalScore;
+    private int distanceScore;
+    public int gradeScore;
+    private int totalGradeScore;
+    public int itemScore;
+    private int totalItemScore;
+    public Text gradeScoreText;
+    public Text itemScoreText;
+    public Text distanceText;
+    public Text totalScoreText;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        if (instance != this)
+        {
+            DestroyImmediate(this);
+        }
+        //DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        timer = 0;
+        gradeScore = 0;
+        totalGradeScore = 0;
+        itemScore = 0;
+        totalScore = 0;
+        gradeScoreText.text = "";
+        itemScoreText.text = "";
+    }
+
+    private void SyncScore()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            timer += Time.deltaTime;
+            distance = Mathf.FloorToInt(timer);
+            distanceScore = distance * 100;
+
+            if (gradeScore != 0)
+            {
+                totalGradeScore += gradeScore;
+                gradeScoreText.text = gradeScore.ToString();
+                gradeScore = 0;
+            }
+            if (itemScore != 0)
+            {
+                totalItemScore += itemScore;
+                itemScoreText.text = itemScore.ToString();
+                itemScore = 0;
+            }
+
+            totalScore = distanceScore + totalGradeScore + totalItemScore;
+
+            distanceText.text = distance.ToString() + "M";
+            totalScoreText.text = totalScore.ToString();
+        }
+    }
+
+    private void Update()
+    {
+        SyncScore();
+    }
+}
