a47c95fa169ef53f3eff10d82848647813416c99
cocococco <kyeonsoo26@gmail.com>
2019-07-08 18:33:03 +0900

    speed 변수 같은걸로 사용하도록 static 수정

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -8,18 +8,19 @@ public class Item : MonoBehaviour
 
     protected int itemScore;
 
-    public float speed = 12; // 직접 입력하지 않도록 수정 필요
-
-    // 쓰는 애들 다 묶어서 구현하면 좋을 듯
-    private void Update()
-    {
-        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - speed * Time.deltaTime);
-    }
+    private float speed;
 
     private void Start()
     {
         itemScore = 0;
         inst_Score = Score.GetInstance();
+        speed = Track.speed;
+    }
+
+    // 쓰는 애들 다 묶어서 구현하면 좋을 듯
+    private void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - speed * Time.deltaTime);
     }
 
     protected void OnCollisionEnter(Collision collision)
--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -6,16 +6,17 @@ public class Obstacle : MonoBehaviour
 {
     private SystemManager inst_SystemManager;
 
-    public float speed = 12; // 직접 입력하지 않도록 수정 필요
-
-    private void Update()
-    {
-        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - speed * Time.deltaTime);
-    }
+    private float speed;
 
     private void Start()
     {
         inst_SystemManager = SystemManager.GetInstance();
+        speed = Track.speed;
+    }
+
+    private void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - speed * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision collision)
