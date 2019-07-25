5c0733c0fe81ee1f1d2b952f8c9d2b40e66be06c
cocococco <kyeonsoo26@gmail.com>
2019-07-26 01:14:23 +0900

    트랙 clone 사용 안하고 돌려쓰도록 수정

--- Assets/Scripts/Track.cs
+++ Assets/Scripts/Track.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Track : MonoBehaviour
 {
     public static float speed = 18;
+    private float startZ = 28;
     private float length;
 
     private SystemManager inst_SystemManager;
@@ -21,24 +22,10 @@ public class Track : MonoBehaviour
         {
             this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - speed * Time.deltaTime);
         }
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
+        if (this.transform.position.z < startZ - length)
         {
-            Debug.Log("player enter in trigger");
-            Vector3 newPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + length);
-            Instantiate(this.gameObject, newPos, Quaternion.identity);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            Debug.Log("player exit in trigger");
-            Destroy(this.gameObject);
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, startZ + length);
         }
     }
 }
